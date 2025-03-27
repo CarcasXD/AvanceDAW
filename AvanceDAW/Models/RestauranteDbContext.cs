bd601e96046731ecbdae2ea9821dc79562d4c031
CarcasXD <carlossalesianoprro@gmail.com>
2025-03-26 23:27:48 -0600

    Agregar archivos de proyecto.

---
+++ AvanceDAW/Models/RestauranteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AvanceDAW.Models
+{
+    public class RestauranteDbContext : DbContext
+    {
+        public RestauranteDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
